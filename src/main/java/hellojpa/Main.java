5aa00b02d7141ac8e057dbecee59705780ef9a7a
yejee1228 <yejee1228@gmail.com>
2021-05-16 00:41:51 +0900

    단방향/ 양방향 매핑

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -1,11 +1,14 @@
 package hellojpa;
 
 import hellojpa.entity.Member;
+import hellojpa.entity.MemberType;
+import hellojpa.entity.Team;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class Main {
     public static void main(String arg[]){
@@ -15,11 +18,29 @@ public class Main {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member= new Member();
-            member.setId(100L);
-            member.setName("안녕");
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
 
+            Member member= new Member();
+            member.setName("hi");
+            member.setTeam(team);
             em.persist(member); //영구저장하다!
+
+            em.flush(); //강제로 DB 반영 시킴 (콘솔에서 DB 반영되는 과정을 보기 위함)
+            em.clear();
+
+            //단방향 매핑
+            Member findMember = em.find(Member.class, member.getId()); //ManytoOne 무조건 Team 테이블을 join 함. fetch=FetchType.LAZY 설정 필요.
+            Team findTeam = findMember.getTeam(); //지연로딩 시, Team을 가져올 때에만 team 테이블을 탐색한다.
+            //양방향 매핑
+            //findTeam = em.find(Team.class,team.getId());
+            findTeam.getName(); //역방향 조회
+
+            List<Member> members = findTeam.getMembers();
+            for(Member member1 : members){
+                System.out.println("member 1 = "+member1);
+            }
             tx.commit();
             System.out.println("Success");
         }catch (Exception e){
--- src/main/java/hellojpa/entity/Member.java
+++ src/main/java/hellojpa/entity/Member.java
@@ -1,13 +1,22 @@
 package hellojpa.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Member {
  @Id //pk와 매핑
+ @GeneratedValue//시퀀스 생성
  private Long id;
+ @Column(name = "USERNAME", nullable = false, length=20)//가장 많이 사용됨.
  private String name;
+ private int age;
+
+ /*@Column(name = "TEAM_ID")
+ private Long teamId;*/
+    @ManyToOne
+    @JoinColumn(name="TEAM_ID")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -25,4 +34,19 @@ public class Member {
         this.name = name;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 }
